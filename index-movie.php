db27e352f3b638d3d31f9728c6ef5f20734a640a
Simone Cardinale <simonecardinale8@gmail.com>
2021-03-12 15:43:22 +0100

    method WIP

--- index-movie.php
+++ index-movie.php
@@ -17,8 +17,8 @@ class Movie {
 
     // metodo
 
-    public function setDurataOre($_durataOre) {
-        $this->durataMinuti / $_durataOre;
+    public function DurataOre($_ore) {
+        $this->durataMinuti/$_ore;
     }
 }
 
@@ -29,6 +29,6 @@ $scarface->durataMinuti = 141;
 $scarface->voto = '9/10';
 $scarface->descrizione = "Il film è il remake dell'omonimo lungometraggio del 1932 diretto da Howard Hawks. A differenza dell'originale ambientato a Chicago durante gli anni del proibizionismo, in questo film l'azione si svolge nella Miami degli anni ottanta, allora centro di un considerevole traffico di droga.";
 
+$scarface->DurataOre('60');
 var_dump($scarface);
-$durataOre = $scarface->setDurataOre('60');
-echo $durataOre;
+
